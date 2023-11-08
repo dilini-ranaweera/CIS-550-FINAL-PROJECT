ea8d4bb50cbdcdd3a7ec115a76d2bb278ea22211
michellechang02 <changmi@seas.upenn.edu>
2023-11-08 12:11:44 -0500

    register

--- client/src/pages/RegistrationPage.js
+++ client/src/pages/RegistrationPage.js
@@ -84,7 +84,7 @@ function RegistrationPage() {
           <Heading as="h2" size="lg" textAlign="center">User Registration</Heading>
           <Stack direction="row" justifyContent="space-between">
             <FormControl id="username" isRequired>
-              <FormLabel>Username</FormLabel>
+              <FormLabel>Email</FormLabel>
               <Input data-testid="username" type="text" value={username} onChange={(e) => usernameChange(e.target.value)} />
             </FormControl>
             <FormControl id="password" isRequired>
@@ -107,25 +107,12 @@ function RegistrationPage() {
               <FormLabel>Full Name</FormLabel>
               <Input data-testid="fullname" type="text" value={name} onChange={(e) => nameChange(e.target.value)} />
             </FormControl>
-            <FormControl id="email" isRequired>
-              <FormLabel>Email</FormLabel>
-              <Input data-testid="email" type="text" value={email} onChange={(e) => emailChange(e.target.value)} />
+            <FormControl id="city" isRequired>
+              <FormLabel>City</FormLabel>
+              <Input data-testid="city" type="text" value={email} onChange={(e) => emailChange(e.target.value)} />
             </FormControl>
           </Stack>
-          <Stack direction="row" justifyContent="space-between">
-            <FormControl id="diet">
-              <FormLabel>Dietary Restrictions</FormLabel>
-              <Input data-testid="diet" type="text" value={diet} onChange={(e) => dietChange(e.target.value)} />
-            </FormControl>
-            <FormControl id="location">
-              <FormLabel>Location</FormLabel>
-              <Input data-testid="location" type="text" value={location} onChange={(e) => locationChange(e.target.value)} />
-            </FormControl>
-          </Stack>
-          <FormControl id="bio">
-            <FormLabel>Bio</FormLabel>
-            <Input data-testid="bio" type="text" value={bio} onChange={(e) => bioChange(e.target.value)} />
-          </FormControl>
+          
           <Stack direction="row" justifyContent="space-between">
             <Button data-testid="register" type="submit" colorScheme="blue">Register</Button>
             <Button data-testid="back" colorScheme="red" onClick={() => navigate('/login')}>Back</Button>
